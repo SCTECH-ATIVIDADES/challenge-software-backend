c976a4f3a269868995ebc9e200c81957bb287558
Wagner Sousa <wagner.fernandes.sousa@outlook.com>
2026-03-05 18:59:42 +0000

    feat: :sparkles: Implementar execução do serviço de sincronização de cidades na inicialização da aplicação

--- src/main/java/com/api/demo/DemoApplication.java
+++ src/main/java/com/api/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.api.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.api.demo.service.CityService;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	private final CityService cityService;
+
+	public DemoApplication(CityService cityService) {
+		this.cityService = cityService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		cityService.syncCities();
 	}
 
 }
