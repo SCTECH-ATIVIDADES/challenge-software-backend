0b2c5701c72d22d38f1b96b3194983c210651c99
Wagner Sousa <wagner.fernandes.sousa@outlook.com>
2026-03-05 18:44:18 +0000

    feat: :sparkles: Adicionar serviço de cidades com sincronização de dados do IBGE

---
+++ src/main/java/com/api/demo/dto/IbgeMunicipioDTO.java
@@ -0,0 +1,23 @@
+package com.api.demo.dto;
+
+public class IbgeMunicipioDTO {
+    private Integer id;
+    private String nome;
+
+    // getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/main/java/com/api/demo/repository/CityRepository.java
+++ src/main/java/com/api/demo/repository/CityRepository.java
@@ -1,5 +1,7 @@
 package com.api.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.api.demo.entity.City;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+    Optional<City> findByCode(Integer code);
 }
